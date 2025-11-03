3dbe5c39a1f448b02716fbcacf83fecba27d6658
sofiamamchyk <sofia.mamchyk@gmail.com>
2025-11-03 06:24:54 +0200

    add env variables for heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -16,7 +17,24 @@ import { AccountModule } from './account/account.module';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('TYPEORM_HOST'),
+        port: 3306,
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: true,
+      }),
+    }),
     TypeOrmModule.forFeature([Product, User, Order]),
     AdminModule,
     AuthModule,
