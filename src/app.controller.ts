2e74e6f1d4a526d476cb86043a42bd43b500c88e
sofiamamchyk <sofia.mamchyk@gmail.com>
2025-11-07 07:50:54 +0200

    enhance navbar layout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,8 +22,6 @@ export class AppController {
     const viewData = [];
     viewData['title'] = 'Про нас - Soft Market';
     viewData['subtitle'] = 'Про нас';
-    viewData['description'] = 'This is an about page ...';
-    viewData['author'] = 'Developed by: Your Name';
     return {
       viewData: viewData,
     };
